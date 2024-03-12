30056c7d484dc5747408a52f1acfe13f4a59f42d
syrshax <92058771+syrshax@users.noreply.github.com>
2024-03-12 18:21:39 +0100

    Update tokenhandler.py

--- modules/tokenhandler.py
+++ modules/tokenhandler.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timedelta
 import requests
 import os
+import logging
 
 class TokenHandler:
     def __init__(self):
@@ -10,7 +11,9 @@ class TokenHandler:
     def getAccessToken(self):
         if datetime.utcnow() >= self.token_expiry:
             self.generateNew()
-            print(f"New token generated: {self.access_token}")
+            #print(f"New token generated: {self.access_token}")
+            logging.info("Access token generated")
+            print(self.access_token)
         return self.access_token
     
     def generateNew(self):
@@ -30,12 +33,17 @@ class TokenHandler:
             self.access_token = data['access_token']
             #10ms/10s/10m... simplemente que no cumpla la hora, sea menor el renew...
             if 'expires_in' in data:
-                self.token_expiry = datetime.utcnow() + timedelta(seconds=data['expires_in']-10)
+                self.token_expiry = datetime.utcnow() + timedelta(seconds=data['expires_in']-30)
         else:
             print("Failed to refresh token:")
             print(f"Status Code: {response.status_code}")
             print(f"Response Body: {response.text}")
             raise Exception(f"Failed to refresh token, server returned status code {response.status_code}")
+        
+
+    
+    def _askForNewRefreshToken(self):
+        pass
 
 #init directo en la class al importar, solo hay 1 instancia
 tokenHandler = TokenHandler()
